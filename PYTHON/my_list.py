16522e9fb3ddbb129df2045ae642ec799cb39aaf
Julietwanza <140821704+Julietwanza@users.noreply.github.com>
2025-08-10 21:41:45 +0300

    Add files via upload

---
+++ PYTHON/my_list.py
@@ -0,0 +1,24 @@
+my_list = []
+
+my_list = [10,20,30,40]
+
+my_list = [10,20,30,40]
+my_list.insert(1,15)
+print(my_list)
+
+my_list = [10,20,30,40]
+another_list = [50,60,70]
+my_list.extend(another_list)
+print(my_list)
+
+my_list =[10,20,30,40,50,60,70]
+my_list.remove(70)
+print(my_list)
+
+my_list =[10,20,30,40,50,60]
+my_list.sort()
+print(my_list)
+
+my_list = [10,20,30,40,50,60]
+index = my_list.index(30)
+print(index)
